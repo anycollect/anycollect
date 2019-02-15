57b2b600d0bd66bfa48123e0d8bd0bc362cc4266
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2019-02-16 01:23:32 +0300

    Fix common-expression module test

--- extensions/common/anycollect-common-expression/src/test/java/io/github/anycollect/extensions/common/expression/std/StdMetricIdBuilderTest.java
+++ extensions/common/anycollect-common-expression/src/test/java/io/github/anycollect/extensions/common/expression/std/StdMetricIdBuilderTest.java
@@ -6,6 +6,7 @@ import io.github.anycollect.extensions.common.expression.EvaluationException;
 import io.github.anycollect.extensions.common.expression.Expression;
 import io.github.anycollect.metric.CommonTags;
 import io.github.anycollect.metric.MetricId;
+import io.github.anycollect.metric.Stat;
 import io.github.anycollect.metric.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,16 +21,19 @@ class StdMetricIdBuilderTest {
     private StdMetricIdBuilder builder;
     private Expression metaTagValueExp;
     private Expression keyValueExp;
+    private Expression statValueExp;
     private Expression unitValueExp;
     private Expression typeValueExp;
 
     @BeforeEach
     void setUp() throws EvaluationException {
         keyValueExp = mock(Expression.class);
+        statValueExp = mock(Expression.class);
         unitValueExp = mock(Expression.class);
         typeValueExp = mock(Expression.class);
         metaTagValueExp = mock(Expression.class);
         when(keyValueExp.process(any())).thenReturn("test");
+        when(statValueExp.process(any())).thenReturn(Stat.max().getTagValue());
         when(unitValueExp.process(any())).thenReturn("tests");
         when(typeValueExp.process(any())).thenReturn(Type.GAUGE.getTagValue());
         when(keyValueExp.process(any())).thenReturn("metric");
@@ -37,6 +41,7 @@ class StdMetricIdBuilderTest {
         builder = new StdMetricIdBuilder(
                 ImmutableMap.of(
                         CommonTags.METRIC_KEY.getKey(), keyValueExp,
+                        CommonTags.STAT.getKey(), statValueExp,
                         CommonTags.UNIT.getKey(), unitValueExp,
                         CommonTags.METRIC_TYPE.getKey(), typeValueExp
                 ),
@@ -49,10 +54,12 @@ class StdMetricIdBuilderTest {
         Args args = Args.builder().build();
         MetricId id = builder.create(args);
         verify(keyValueExp, times(1)).process(args);
+        verify(statValueExp, times(1)).process(args);
         verify(unitValueExp, times(1)).process(args);
         verify(typeValueExp, times(1)).process(args);
         verify(metaTagValueExp, times(1)).process(args);
         assertThat(id.getKey()).isEqualTo("metric");
+        assertThat(id.getStat()).isEqualTo(Stat.max());
         assertThat(id.getUnit()).isEqualTo("tests");
         assertThat(id.getType()).isEqualTo(Type.GAUGE);
         assertThat(id.getMetaTagValue("metaTag")).isEqualTo("meta");
