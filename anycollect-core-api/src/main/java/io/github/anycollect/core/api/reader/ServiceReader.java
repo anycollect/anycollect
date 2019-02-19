dc8f70a6732e729442555ebb6cd0f8b1c0f96cd5
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2019-02-20 00:34:48 +0300

    Make service reader extension point

--- anycollect-core-api/src/main/java/io/github/anycollect/core/api/reader/ServiceReader.java
+++ anycollect-core-api/src/main/java/io/github/anycollect/core/api/reader/ServiceReader.java
@@ -3,9 +3,11 @@ package io.github.anycollect.core.api.reader;
 import io.github.anycollect.core.api.common.Lifecycle;
 import io.github.anycollect.core.api.common.Plugin;
 import io.github.anycollect.core.api.dispatcher.Dispatcher;
+import io.github.anycollect.extensions.annotations.ExtPoint;
 
 import javax.annotation.Nonnull;
 
+@ExtPoint
 public interface ServiceReader extends Plugin, Lifecycle {
     void start(@Nonnull Dispatcher dispatcher);
 }
