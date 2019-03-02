9829290809b17a57ba031ad492afb9f7f937e1f6
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2019-03-02 21:25:56 +0300

    Fix Deserializer interface

--- anycollect-core-api/src/main/java/io/github/anycollect/core/api/Deserializer.java
+++ anycollect-core-api/src/main/java/io/github/anycollect/core/api/Deserializer.java
@@ -1,12 +1,10 @@
 package io.github.anycollect.core.api;
 
-import io.github.anycollect.extensions.annotations.ExtPoint;
-import io.github.anycollect.metric.Metric;
+import io.github.anycollect.metric.MetricFamily;
 
 import javax.annotation.Nonnull;
 
-@ExtPoint
 public interface Deserializer {
     @Nonnull
-    Metric deserialize(@Nonnull String source);
+    MetricFamily deserialize(@Nonnull String source);
 }
