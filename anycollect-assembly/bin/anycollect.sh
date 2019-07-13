2c863f2bf6d6aa8f28347f733687a1a091bccd39
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2019-07-14 01:18:30 +0300

    Add jfr and debug support Add run action Add tools.jar to classpath (for future attach java plugin)

--- anycollect-assembly/bin/anycollect.sh
+++ anycollect-assembly/bin/anycollect.sh
@@ -1,26 +1,94 @@
 #!/bin/bash
 
+ACTION=status
+ENABLE_JFR=false
+ENABLE_DEBUG=false
+DEBUG_PORT=5005
+while (( "$#" )); do
+    case "$1" in
+        -a|--additional-anycollect-args)
+            ADDITIONAL_ANYCOLLECT_ARGS=$2
+            shift 2
+            ;;
+        --enable-jfr)
+            ENABLE_JFR=$2
+            shift 2
+            ;;
+        --debug)
+            ENABLE_DEBUG=true
+            DEBUG_PORT=$2
+            shift 2
+            ;;
+        --start)
+            ACTION=start
+            shift
+            ;;
+        --status)
+            ACTION=status
+            shift
+            ;;
+        --stop)
+            ACTION=stop
+            shift
+            ;;
+        --restart)
+            ACTION=restart
+            shift
+            ;;
+        --run)
+            ACTION=run
+            shift
+            ;;
+        --) # end argument parsing
+            shift
+            break
+            ;;
+        -*|--*=) # unsupported flags
+            echo "Error: Unsupported flag $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
 # Java
 JAVA_HOME=${JAVA_HOME:-"/usr"}
 ANYCOLLECT_JAR=${ANYCOLLECT_JAR:-"./lib/anycollect.jar"}
 JAVA_OPTS=${JAVA_OPTS:-""}
-ANYCOLLECT_EXT=${ANYCOLLECT_EXT:-"./extensions/*"}
-CLASSPATH=${CLASSPATH:-"${ANYCOLLECT_EXT}"}
 JAVA=${JAVA:-"${JAVA_HOME}/bin/java"}
 
-# pid
-JPS=${JPS:-"${JAVA_HOME}/bin/jps -l"}
-PID_FILE=${PID_FILE:-"./var/run/anycollect.pid"}
-PID_EXEC="$JPS | grep anycollect | awk '{print \$1};'"
+# Java Flight Recorder
+if [[ ${ENABLE_JFR} = true ]]; then
+    JFR_OPTS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
+    JAVA_OPTS="${JAVA_OPTS} ${JFR_OPTS}"
+fi
 
+# Debug
+if [[ ${ENABLE_DEBUG} = true ]]; then
+    DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${DEBUG_PORT}"
+    JAVA_OPTS="${JAVA_OPTS} ${DEBUG_OPTS}"
+fi
+
+# Class-Path
+ANYCOLLECT_EXT=${ANYCOLLECT_EXT:-"./extensions/*"}
+TOOLS_JAR="${JAVA_HOME}/lib/tools.jar"
+CLASSPATH=${CLASSPATH:-"${TOOLS_JAR}:${ANYCOLLECT_EXT}:${ANYCOLLECT_JAR}"}
+
+# pid
+PID_FILE=${PID_FILE:-"./var/run/anycollect.pid"}
+
+# AnyCollect
 LOG_DIR=${LOG_DIR:-"./logs"}
 LOG_LEVEL=${LOG_LEVEL:-"debug"}
 ANYCOLLECT_OPTS=${ANYCOLLECT_OPTS:-"-Danycollect.log.dir=${LOG_DIR} -Danycollect.log.level=${LOG_LEVEL}"}
 ANYCOLLECT_CONF_FILE=${ANYCOLLECT_CONF_FILE:-"./etc/anycollect.yaml"}
 LOGBACK_CONF_FILE=${LOGBACK_CONF_FILE:-"./etc/logback.xml"}
-ANYCOLLECT_ARGS=${ANYCOLLECT_ARGS:-"--conf=${ANYCOLLECT_CONF_FILE} --logback-conf=${LOGBACK_CONF_FILE} --pid-file=${PID_FILE}"}
+ADDITIONAL_ANYCOLLECT_ARGS=${ADDITIONAL_ANYCOLLECT_ARGS:-""}
+ANYCOLLECT_ARGS=${ANYCOLLECT_ARGS:-"--logback-conf=${LOGBACK_CONF_FILE} --pid-file=${PID_FILE} ${ADDITIONAL_ANYCOLLECT_ARGS}"}
+MAIN_CLASS="io.github.anycollect.Init"
 
 getAnyCollectPid() {
+    JPS=${JPS:-"${JAVA_HOME}/bin/jps -l"}
+    PID_EXEC="$JPS | grep anycollect | awk '{print \$1};'"
     echo $(eval ${PID_EXEC})
 }
 
@@ -33,14 +101,26 @@ HEAP_INIT=${HEAP_INIT:-"256m"}
 HEAP_MAX=${HEAP_MAX:-"512m"}
 GC_OPTS=${GC_OPTS:-"-Xms${HEAP_INIT} -Xmx${HEAP_MAX}"}
 
+createExecLine() {
+    echo "${JAVA} -server ${JAVA_OPTS} ${GC_OPTS} ${MONITOR_OPTS} ${ANYCOLLECT_OPTS} -cp ${CLASSPATH} ${MAIN_CLASS} ${ANYCOLLECT_ARGS}"
+}
 
-start() {
+assertAnyCollectIsNotRunning() {
     pid=$(getAnyCollectPid)
     if [[ ! -z ${pid} ]]; then
         echo "AnyCollect is running, pid ${pid}"
         exit 1
     fi
-    nohup ${JAVA} -server ${JAVA_OPTS} ${GC_OPTS} ${MONITOR_OPTS} ${ANYCOLLECT_OPTS} -cp ${CLASSPATH} -jar ${ANYCOLLECT_JAR} ${ANYCOLLECT_ARGS} &
+}
+
+start() {
+    assertAnyCollectIsNotRunning
+    nohup sh -c "$(createExecLine) &"
+}
+
+run() {
+    assertAnyCollectIsNotRunning
+    sh -c "$(createExecLine)"
 }
 
 status() {
@@ -59,7 +139,7 @@ stop() {
         while (true); do
             ps -p ${pid} >/dev/null
             if [[ $? -eq 0 ]]; then
-                echo "Waiting for graceful shutdown"
+                echo "Waiting for graceful shutdown, pid=${pid}"
             else
                 echo "AnyCollect has been stopped gracefully"
                 break
@@ -76,7 +156,7 @@ restart() {
     start
 }
 
-case $1 in
+case ${ACTION} in
     start)
         start
     ;;
@@ -86,11 +166,14 @@ case $1 in
     restart)
         restart
     ;;
+    run)
+        run
+    ;;
     status)
         status
     ;;
     *)
-        echo $"Usage: $0 {start|stop|restart|status}"
+        echo $"Usage: $0 {start|stop|restart|run|status}"
     ;;
 esac
 
